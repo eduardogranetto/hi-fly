b2927b9c06d92d7b8f382e9f5f2dd5a450ae6737
Eduardo Granetto <eduardo.granetto@anfcorp.com>
2023-06-23 13:58:11 -0300

    Fly deploy

--- src/main/kotlin/io/test/hifly/RootController.kt
+++ src/main/kotlin/io/test/hifly/RootController.kt
@@ -3,6 +3,7 @@ package io.test.hifly
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import java.lang.System.getenv
 
 
 @RestController
@@ -11,8 +12,8 @@ class RootController {
     @GetMapping("/")
     fun root() = Mono.just(
             mapOf(
-                    "code" to "hi"
-
+                    "code" to "hi",
+                    "hiVar" to (getenv("HI_VAR") ?: "UNKNOWN")
             )
     )
 
